f909faaca1700e9ef24b780476a04a8452dc5941
nitunwa <nitun.2@gmail.com>
2020-04-24 12:04:35 -0700

    add api call successful

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,8 +1,11 @@
 // business logic
 export class CurrencyService {
+  
+  constructor(){}
+  
   async getCurrency() {
     try {
-      let response = await fetch(``);
+      let response = await fetch(`https://prime.exchangerate-api.com/v5/${process.env.API_KEY}/latest/USD`);
       let jsonifiedResponse;
       if (response.ok && response.status == 200) {
         jsonifiedResponse = await response.json();
--- src/main.js
+++ src/main.js
@@ -1,23 +1,30 @@
 // user interface logic
 
 import $ from "jquery";
-import { CurrencyServicee } from './../src/currency-service.js';
+import { CurrencyService } from './../src/currency-service.js';
 
 $(document).ready(function(){
   $("#currencyInfo").click(function(){
-
+    
+    let amountUSD = $("#amount").val();
+    let typeOfCurrency = $("#convertCurrency").val();
 
     (async () => {
-      let currencyService = new CurrencyServicee();
+      let currencyService = new CurrencyService();
       const response = await currencyService.getCurrency();
+      console.log("val");
+      console.log("respons " + response);
       getResponse(response);
-    })();
+     })();
 
     function getResponse(response){
       if (response){
-
+        let totalUSD = response.conversion_rates.USD * amountUSD;
+        console.log(totalUSD+" -------------------")
+        let finalCurrency = totalUSD * `response.conversion_rates.${typeOfCurrency}`;
+        $(".display").text("success " + finalCurrency);   
       }else{
-        
+        $(".error").text("error");
       }
     }
 
