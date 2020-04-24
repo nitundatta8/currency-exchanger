8c544ee50a48cf11934deaf31945c0c0bf77cacd
nitunwa <nitun.2@gmail.com>
2020-04-24 10:37:08 -0700

    add business logic to currency-service js file

--- src/currency-service.js
+++ src/currency-service.js
@@ -1,2 +1,19 @@
 // business logic
+export class CurrencyService {
+  async getCurrency() {
+    try {
+      let response = await fetch(``);
+      let jsonifiedResponse;
+      if (response.ok && response.status == 200) {
+        jsonifiedResponse = await response.json();
+      } else {
+        jsonifiedResponse = false;
+      }
+      return jsonifiedResponse;
+    } catch(error) {
+      return false;
+    }
+  }
+
+}
 
